ea66ba05acc67420dd2ee7d03e052a20e82a7bb1
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-17 19:27:34 +0000

    add images and create image array

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("check");
+
+let images = [
+  { name: "rose", imgSrc: "./images/roses.jpg", id: 1 },
+  { name: "carnation", imgSrc: "./images/carnations.jpg", id: 2 },
+  { name: "lily", imgSrc: "./images/lilies.jpg", id: 3 },
+  { name: "iris", imgSrc: "./images/iris.jpg", id: 4 },
+  { name: "marigold", imgSrc: "./images/marigold.jpg", id: 5 },
+  { name: "orchid", imgSrc: "./images/orchids.jpg", id: 6 },
+  { name: "tulip", imgSrc: "./images/tulips.jpg", id: 7 },
+  { name: "daisy", imgSrc: "./images/ldaisies.jpg", id: 8 },
+  { name: "rose", imgSrc: "./images/roses.jpg", id: 9 },
+  { name: "carnation", imgSrc: "./images/carnations.jpg", id: 10 },
+  { name: "lily", imgSrc: "./images/lilies.jpg", id: 11 },
+  { name: "iris", imgSrc: "./images/iris.jpg", id: 12 },
+  { name: "marigold", imgSrc: "./images/marigold.jpg", id: 13 },
+  { name: "orchid", imgSrc: "./images/orchids.jpg", id: 14 },
+  { name: "tulip", imgSrc: "./images/tulips.jpg", id: 15 },
+  { name: "daisy", imgSrc: "./images/ldaisies.jpg", id: 16 },
+];
